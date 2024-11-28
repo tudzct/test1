5e1acb107a8395fb07c90868f70a7286aaadf1e9
Ly Duc Tu <direct.ldt@gmail.com>
2024-11-28 15:07:33 +0700

    add 3 cpp files

---
+++ ldt.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << "My name is Tu" << std::endl;
+    }
+
+    return 0;
+}
---
+++ mp.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "mai phuong" << std::endl;
+    return 0;
+}
