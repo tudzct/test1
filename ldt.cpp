5bb1eff804c78396744f3bb69033b61e628d1ac4
Ly Duc Tu <direct.ldt@gmail.com>
2024-11-28 15:22:39 +0700

    delete stuff

--- ldt.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    for (int i = 0; i < 5; i++) {
-        std::cout << "My name is Tu" << std::endl;
-    }
-
-    return 0;
-}
--- mp.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "mai phuong" << std::endl;
-    return 0;
-}
